ff9bcbfaa4bd33e5d0b2a0889b47c538aecc6c7f
OkothPius <orukopius8@gmail.com>
2022-07-20 23:56:28 +0300

    feat: update

--- masomo/settings.py
+++ masomo/settings.py
@@ -125,6 +125,4 @@ MEDIA_URL = '/media/'
 CART_SESSION_ID = 'cart'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
