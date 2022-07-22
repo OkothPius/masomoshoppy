5726b02a12310f391cfbea5ebe35a72c74960037
OkothPius <orukopius8@gmail.com>
2022-07-22 14:51:44 +0300

    feat: upate

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from apps.cart.forms import CartAddProductForm
-from .models import Category, Product
-
-def index(request):
-    return render(request, 'core/index.html')
+from .models import Category, Product, Type
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    types = Type.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    context = {'category': category, 'categories': categories, 'products': products}
-    return render(request, 'shop/product/list.html', context)
+    context = {'category': category, 'categories': categories,
+                'products': products, 'types': types}
+    return render(request, 'core/index.html', context)
 
 
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    cart_product_form = CartAddProductForm()
-    context = {'product': product, 'cart_product_form': cart_product_form}
-    return render(request, 'shop/product/detail.html', context)
-
+# def product_detail(request, id, slug):
+#     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+#     cart_product_form = CartAddProductForm()
+#     context = {'product': product, 'cart_product_form': cart_product_form}
+#     return render(request, 'core/product/detail.html', context)
