0dce809a2e7ad92d59bb5ddd19a47d6760c1d4c3
OkothPius <orukopius8@gmail.com>
2022-07-23 01:53:53 +0300

    feat: update

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,5 +6,14 @@ app_name = 'core'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('books/', views.book, name='books_list'),
+    path('uniforms/', views.uniform, name='uniforms_list'),
+    path('stationary/', views.stationary, name='stationary_list'),
+    path('electronic/', views.electronic, name='electronic_list'),
+    path('equipment/', views.equipment, name='equipment_list'),
+
+
+
+
     # path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
