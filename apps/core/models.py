e7a4033b4cafc24f4b7cd7999d29d93a6830fded
OkothPius <orukopius8@gmail.com>
2022-07-22 12:51:32 +0300

    feat: update uniforms sizes

--- apps/core/models.py
+++ apps/core/models.py
@@ -13,8 +13,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('shop:product_list_by_category', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 class Type(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -27,94 +27,19 @@ class Type(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
-
-    def __str__(self):
-        return self.name
-
-
-class Stationary(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
-
-    def __str__(self):
-        return self.name
-
-
-class Uniform(models.Model):
+class Product(models.Model):
     UNIFORM_SIZES = (
         ('S', 'Small'),
         ('M', 'Medium'),
         ('L', 'Large'),
         ('XL', 'Extra Large'),
     )
-    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200, db_index=True)
-    uniform_size = models.CharField(max_length=2, choices=UNIFORM_SIZES)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
-
-    def __str__(self):
-        return self.name
-
-
-class Electronic(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
-
-    def __str__(self):
-        return self.name
-
-
-class Equipment(models.Model):
+    
     category = models.ForeignKey(Category, related_name='products',
                                  on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200, db_index=True)
+    uniform_size = models.CharField(max_length=2, choices=UNIFORM_SIZES, null=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
@@ -130,14 +55,8 @@ class Equipment(models.Model):
     def __str__(self):
         return self.name
 
-# class Review(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product , on_delete=models.CASCADE)
-#     date = models.DateTimeField(auto_now_add=True)
-#     text = models.TextField(max_length=3000 , blank=True)
-#     rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
-#     likes= models.PositiveIntegerField(default=0)
-#     dislikes = models.PositiveIntegerField(default=0)
-#
-#     def __str__(self):
-#         return self.user.full_name
+    # def type():
+    #     return 'category type'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
