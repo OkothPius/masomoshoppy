ca88f8923a6603efa7bb3b01c78af21813d47940
OkothPius <orukopius8@gmail.com>
2022-07-23 01:53:39 +0300

    fix: reverse error

--- apps/core/models.py
+++ apps/core/models.py
@@ -59,4 +59,4 @@ class Product(models.Model):
     #     return 'category type'
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse('core:index')
